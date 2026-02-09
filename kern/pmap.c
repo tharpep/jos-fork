bc44f2e4ee205358501e4c25eb7508ead035e7c7
Pryce Tharpe <pjtharpe04@gmail.com>
2026-02-09 16:29:58 -0500

    added page lookup

--- kern/pmap.c
+++ kern/pmap.c
@@ -484,7 +484,15 @@ struct PageInfo *
 page_lookup(pde_t *pgdir, void *va, pte_t **pte_store)
 {
 	// Fill this function in
-	return NULL;
+	// LLM-Assisted with this code
+	pte_t *pte = pgdir_walk(pgdir, va, 0);
+
+	if (!pte || !(*pte & PTE_P))
+		return NULL;
+	if (pte_store)
+		*pte_store = pte;
+
+	return pa2page(PTE_ADDR(*pte));
 }
 
 //
