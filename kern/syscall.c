5e54103af3e1f0ca54b6c7e6fb534ae3e675eb03
Pryce Tharpe <pjtharpe04@gmail.com>
2026-04-06 15:35:47 -0400

    exercise 8 first pass

--- kern/syscall.c
+++ kern/syscall.c
@@ -145,7 +145,15 @@ sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
 	// LAB 4: Your code here.
 	// llm-Assisted code
-	panic("sys_env_set_pgfault_upcall not implemented");
+	struct Env *e;
+	int r;
+
+	r = envid2env(envid, &e, 1);
+	if (r < 0)
+		return r;
+
+	e->env_pgfault_upcall = func;
+	return 0;
 }
 
 // Allocate a page of memory and map it at 'va' with permission
@@ -395,6 +403,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return sys_page_map(a1, (void *) a2, a3, (void *) a4, a5);
 	case SYS_page_unmap:
 		return sys_page_unmap(a1, (void *) a2);
+	case SYS_env_set_pgfault_upcall:
+		return sys_env_set_pgfault_upcall(a1, (void *) a2);
 	default:
 		return -E_INVAL;
 	}
