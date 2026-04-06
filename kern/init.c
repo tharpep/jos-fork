afa6f9318301133c422c3e73d2ace60256e19e52
Pryce Tharpe <pjtharpe04@gmail.com>
2026-04-06 15:20:09 -0400

    exercise 5* first pass

--- kern/init.c
+++ kern/init.c
@@ -43,6 +43,8 @@ i386_init(void)
 
 	// Acquire the big kernel lock before waking up APs
 	// Your code here:
+	// llm-Assisted code
+	lock_kernel();
 
 	// Starting non-boot CPUs
 	boot_aps();
@@ -109,9 +111,8 @@ mp_main(void)
 	// only one CPU can enter the scheduler at a time!
 	//
 	// Your code here:
-
-	// Remove this after you finish Exercise 6
-	for (;;);
+	lock_kernel();
+	sched_yield();
 }
 
 /*
