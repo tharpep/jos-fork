cba51a516fec10335cc3ded54503571dbf391524
Pryce Tharpe <pjtharpe04@gmail.com>
2026-04-06 14:53:25 -0400

    exercise 2 first pass

--- kern/pmap.c
+++ kern/pmap.c
@@ -335,6 +335,11 @@ page_init(void)
 	pages[0].pp_link = NULL;
 
 	for (i = 1; i < npages_basemem; i++) {
+		if (i == MPENTRY_PADDR / PGSIZE) {
+			pages[i].pp_ref = 1;
+			pages[i].pp_link = NULL;
+			continue;
+		}
 		pages[i].pp_ref = 0;
 		pages[i].pp_link = page_free_list;
 		page_free_list = &pages[i];
