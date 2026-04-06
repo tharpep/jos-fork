50ca7c2644a092b113581fab814adc0cb12887e8
Pryce Tharpe <pjtharpe04@gmail.com>
2026-04-06 14:59:23 -0400

    exercise 3 first pass

--- kern/pmap.c
+++ kern/pmap.c
@@ -288,7 +288,16 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
-
+	// LLM-Assisted with this code
+	int i;
+	for (i = 0; i < NCPU; i++) {
+		uintptr_t kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+		boot_map_region(kern_pgdir,
+			kstacktop_i - KSTKSIZE,
+			KSTKSIZE,
+			PADDR(percpu_kstacks[i]),
+			PTE_W);
+	}
 }
 
 // --------------------------------------------------------------
